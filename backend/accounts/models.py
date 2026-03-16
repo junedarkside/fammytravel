f08537ced87b0c325934219a2ef9fa5991d0d7a3
junedarkside <june_pinkfloyd@hotmail.com>
2026-03-16 11:16:13 +0700

    resolved account app in backend

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,32 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    """Custom user manager where email is the unique identifier"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular user with the given email and password"""
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a superuser with the given email and password"""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -8,6 +35,7 @@ class User(AbstractUser):
     Uses email as username (required), not username
     """
     username = None
+    objects = UserManager()
     email = models.EmailField(unique=True, blank=False, null=False)
     phone = models.CharField(max_length=20, blank=True, null=True)
     role = models.CharField(
